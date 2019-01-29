1c5b5801fb6c5dfa45955ced81adc11ffdd30857
giongto35 <giongto35@yahoo.com>
2019-01-30 02:51:29 +0800

    Update leaderboard text

--- client/src/sprites/Leaderboard.js
+++ client/src/sprites/Leaderboard.js
@@ -10,13 +10,14 @@ export default class extends Phaser.Sprite {
 
     var rect = new Phaser.Graphics(game, 0, 0);
     rect.lineStyle(2, 0x000000);
-    rect.beginFill(0x000000, 1);
+    rect.beginFill(0xFFFFFF, 1);
     rect.drawRect(config.screenWidth - config.leaderboardSize, 0, config.leaderboardSize, this.numTop * 25 + 10);
     rect.alpha = 0.3;
     this.addChild(rect);
 
     for (var i = 0; i < this.numTop; i++) {
       var text = game.add.text(config.screenWidth - config.leaderboardSize + 10, i * 25, '');
+      text.fill = '#ffffff';
       text.bringToTop();
       this.topList.push(text);
       this.addChild(text);
