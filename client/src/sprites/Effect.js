3b324ed66156bf5a99e35fad1777cae005868b4b
giongto35 <giongto35@yahoo.com>
2019-01-30 22:59:29 +0800

    Update effect

--- client/src/sprites/Effect.js
+++ client/src/sprites/Effect.js
@@ -5,10 +5,10 @@ export function explode (game, layer, x, y) {
   emitter.makeParticles('explode_particle');
   emitter.gravity = 0;
   emitter.setScale(0.2, 0.1, 0.2, 0.1, 500, Phaser.Easing.Linear.None);
-  emitter.setAlpha(0.9, 0.6, 500, Phaser.Easing.Linear.None, false);
+  emitter.setAlpha(0.7, 0.3, 500, Phaser.Easing.Linear.None, false);
   emitter.setXSpeed(-1000, 1000);
   emitter.setYSpeed(-1000, 1000);
-  emitter.explode(300, 200);
+  emitter.explode(400, 200);
   layer.add(emitter);
 
   game.time.events.add(200, function () { emitter.destroy(); }, this);
